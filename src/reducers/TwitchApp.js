5021faae10759e3d1d7d6e1b0cd5c16e23bf414a
randal4 <randal4@Davids-MacBook-Pro.local>
2018-07-31 20:59:13 -0400

    linter 3

--- src/reducers/TwitchApp.js
+++ src/reducers/TwitchApp.js
@@ -1,31 +1,28 @@
 const initialState = {
-  status: "",
+  status: '',
   streams: [],
-  error: ""
-}
+  error: ''
+};
 
 function TwitchApp(state=initialState, action){
   switch(action.type){
     case 'FETCH_REQUEST':
-      const requested = Object.assign({}, state, {
+      return Object.assign({}, state, {
         status: action.status
       });
-      return requested;
     case 'FETCH_SUCCESS':
-      const successful = Object.assign({}, state, {
+      return  Object.assign({}, state, {
         status: action.status,
         streams: action.streams
       });
-      return successful;
     case 'FETCH_FAILURE':
-      const failed = Object.assign({}, state, {
+      return  Object.assign({}, state, {
         status: action.status,
         error: action.error
       });
-      return failed;
     default:
       return state;
   }
 }
 
-export default TwitchApp
+export default TwitchApp;
